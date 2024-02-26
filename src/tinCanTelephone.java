ecb9da370fe21904357d25b571621f5da1f9fe1f
Hunter Wotherspoon <hwotherspoon2025@crescentschool.org>
2024-02-26 11:10:30 -0500

    figured out storing higest and lowest, not sure how to compare to null

--- src/tinCanTelephone.java
+++ src/tinCanTelephone.java
@@ -6,6 +6,10 @@ public class tinCanTelephone {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		//2000 because it can be from -1000 to 1000
+		int[] lowestY=new int[2000],highestY= new int[2000];
+
 		//X coordinate of Romy's window, 0 in test case
 		int xR = Integer.parseInt(st.nextToken());
 	  //Y coordinate of Romy's window, 0 in test case
@@ -14,16 +18,33 @@ public class tinCanTelephone {
 		int xJ = Integer.parseInt(st.nextToken());
 		//Y coordinate of Jules window, 3 in test case
 		int yJ = Integer.parseInt(st.nextToken());
+
 		//Number of buildings, 1 in test case
 		int numBuildings = Integer.parseInt(br.readLine());
+
 	 	//Loop through how many buildings there are
 		for (int i = 0; i < numBuildings; i++) {
+
 			// Redeclare stringTokenizer for a new Line
 			st = new StringTokenizer(br.readLine());
 			// Number of corners the current building has
 			int numCorners = Integer.parseInt(st.nextToken());
+
 			//For loop for the number of corners
 			for(int j=0;j<numCorners;j++){
+				int curX = Integer.parseInt(st.nextToken());
+				int curY = Integer.parseInt(st.nextToken());
+
+				if(lowestY[curX]==null){
+
+				}
+
+				//use an array
+
+				//loop from -1000 to 1000
+				//store lowest point
+				//store highest point
+				//for each column
 				
       }
 		}
